1587f1c9d2ff04324649bd226bbf4b247e1c15eb
Tosh <adevkota@pamperedchef.com>
2018-02-25 19:03:36 -0600

    game.js tests

--- test/modules/game.spec.js
+++ test/modules/game.spec.js
@@ -1,15 +1,18 @@
 import {Game} from "../../src/modules/game";
 describe("Game", function() {
    let game;
+   let board;
    beforeEach(() => {
       game = new Game();
-      spyOn(game, "init");
+      board = game.board
+      spyOn(board, "init");
    })
    it("should have new board", () => {
       expect(game.board).toBeDefined();
    });
 
    it("init should initialize the game's board", () => {
-
+      game.init();
+      expect(board.init).toHaveBeenCalled();
    })
 })
